b1d37f725ea1a2e45ffb7b73d46393ec28035963
younghoon <digitcomgisul@gmail.com>
2021-07-16 08:57:57 +0900

    admin 구성
    admin 구성

--- user/models.py
+++ user/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    emaiil = models.EmailField(verbose_name='이메일')
+    email = models.EmailField(verbose_name='이메일')
     password = models.CharField(max_length=64, verbose_name='비밀번호')
     regiter_date = models.DateTimeField(auto_now_add=True, verbose_name='등록일자')
 
